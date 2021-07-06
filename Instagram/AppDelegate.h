80d6a51612a4dd9ed4b00949e5dff504e907397f
Jacqueline DiMonte <jdimonte@fb.com>
2021-07-06 11:40:06 -0700

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Jacqueline DiMonte on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
